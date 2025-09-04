938be8d4e947ef667869efcdf0889676f054f2db
julien37170 <julien37170@gmail.com>
2025-09-04 07:21:04 -0700

    Add Foundry global declarations

---
+++ systems/cof-v2/src/module/foundry.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Global declarations for Foundry VTT APIs. These are declared as any so that
+ * TypeScript can compile the system source without type errors. In a full
+ * project you should install @types/foundry or provide proper typings.
+ */
+declare const Hooks: any;
+declare const Actors: any;
+declare const Items: any;
+declare const ActorSheet: any;
+declare const ItemSheet: any;
+declare const Roll: any;
+declare const ChatMessage: any;
+declare const mergeObject: any;
+declare const Dialog: any;
+declare const ui: any;
+declare const game: any;
+declare const canvas: any;
